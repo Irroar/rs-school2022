6c4c060444160b964c9ca14449d81f3a984213af
Iryna Lytvynenko <frau4145@gmail.com>
2022-07-07 19:32:35 +0300

    feat: add burger menu

--- migration-newip-to-ts/src/components/app/app.ts
+++ migration-newip-to-ts/src/components/app/app.ts
@@ -15,7 +15,19 @@ class App {
         (document.querySelector('.sources') as HTMLElement).addEventListener('click', (e: MouseEvent) =>
             this.controller.getNews(e, (data: IData) => this.view.drawNews(data))
         );
+        (document.querySelector('.burger') as HTMLElement).addEventListener('click', (): void => {
+            this.toggleBurger();
+        });
         this.controller.getSources((data: ISources) => this.view.drawSources(data));
+    }
+
+    private toggleBurger(): void {
+        const burger: HTMLElement = document.querySelector('.burger') as HTMLElement;
+        const sources: HTMLElement = document.querySelector('.sources') as HTMLElement;
+        if (getComputedStyle(burger).display === 'block') {
+            burger.classList.toggle('active');
+            sources.classList.toggle('active');
+        }
     }
 }
 
--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '29d5a9bac2f34d79ab91f0e08c63ad6d',
         });
     }
